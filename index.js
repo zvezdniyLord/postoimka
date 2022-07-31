f8026368acef512b4c14c026201052064f09f50a
Aleksandr Goynik <77444353+228SxS228@users.noreply.github.com>
2022-07-31 10:26:07 +0700

    Добаваил остановку таймера

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const btnStart = document.getElementById('btnStart');
-const divTime = document.getElementById('divTime')
-const startTime = function() {
+const divTime = document.getElementById('divTime');
+const btnEnd = document.getElementById('btnEnd');
+
+let timer;
+
+btnStart.addEventListener('click', function() {
   let countTime = 0;
-  setInterval(() => {
+  timer = setInterval( () =>{
     if(countTime == 60) countTime = 0;
     countTime += 1;
     divTime.innerHTML = countTime;
   }, 1000);
-}
+});
+
+btnEnd.addEventListener('click', () => {
+  if(timer){
+    clearTimeout(timer);
+  }
+});
+
+// const startTime = function() {
+//   let countTime = 0;
+//   setInterval(() => {
+//     if(countTime == 60) countTime = 0;
+//     countTime += 1;
+//     divTime.innerHTML = countTime;
+//   }, 1000);
+// }
+
+// const endTime = function() {
+//   clearInterval(countTime);
+// };
 
 
-btnStart.addEventListener("click", startTime);
+// btnStart.addEventListener("click", startTime);
+// btnEnd.addEventListener("click", endTime);
